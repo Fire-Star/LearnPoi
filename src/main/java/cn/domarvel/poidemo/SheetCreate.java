9e997f88048a75bbdfee8cf9986270a4d73b517a
Fire-Star <1428384239@qq.com>
2017-10-18 19:32:28 +0800

    完善创建表后，表格文字居中对齐，并且自动调整列宽！

--- src/main/java/cn/domarvel/poidemo/SheetCreate.java
+++ src/main/java/cn/domarvel/poidemo/SheetCreate.java
@@ -1,11 +1,15 @@
 package cn.domarvel.poidemo;
 
+import cn.domarvel.utils.ExcelUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.Test;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SheetCreate {
     public static void main(String[] args) throws IOException {
@@ -15,5 +19,23 @@ public class SheetCreate {
         OutputStream outputStream = new FileOutputStream("K:\\文件上传\\Sheet演示.xls");
         wb.write(outputStream);
         outputStream.close();
+    }
+
+    @Test
+    public void createSheetByUtils() throws Exception {
+        String []title = {"姓名","简介"};
+        List<List<String>> insertData = new LinkedList<>();
+
+        List<String> tempData01 = new LinkedList<>();
+        tempData01.add("FireLang");
+        tempData01.add("很久很久以前，少年对黑客痴迷！");
+        insertData.add(tempData01);
+
+        List<String> tempData02 = new LinkedList<>();
+        tempData02.add("MoonFollow");
+        tempData02.add("在路途中，少年遇到了 Joy 并且深深深深的爱上了她！");
+        insertData.add(tempData02);
+
+        ExcelUtils.createExcel("K:\\文件上传\\","简介.xlsx",insertData,"简介",title,1,1);
     }
 }
--- src/main/java/cn/domarvel/utils/ExcelUtils.java
+++ src/main/java/cn/domarvel/utils/ExcelUtils.java
@@ -6,8 +6,8 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 /**
@@ -17,9 +17,10 @@ import java.util.List;
  * Time: 12:45
  * Excel工具类：所有方法都经过深思熟虑实现，扩展强，功能实现具体，BUG 少，结构堪称完美！！！
  *
- * 都是为了让你少写代码，不！少加班！
+ * 都是为了让你写出优雅的代码，不！是少加班！
  */
 public class ExcelUtils {
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
     /**
      *
      * @param bathPath 要类似于这样的路径：c:/base/path/
@@ -29,7 +30,7 @@ public class ExcelUtils {
      * @param titleName 标题名数组
      * @param startX 整个表的起始位置X
      * @param startY 整个表的起始位置Y
-     * @throws IOException
+     * @throws Exception 异常
      */
     public static void createExcel(String bathPath, String fileName, List<List<String>> insertData, String sheetName, String titleName[], int startX, int startY) throws Exception {
         XSSFWorkbook wb = new XSSFWorkbook();
@@ -47,14 +48,27 @@ public class ExcelUtils {
         //设置标题行内容
         for (int i = 0; i < titleName.length; i++) {
             String tempTitle = titleName[i];
-            Cell tempCell = titleRow.createCell(i+startX);
+
+            if(tempTitle == null){
+                continue;
+            }
+            int startIndexColumn = i+startX;//计算开始插入列
+            Cell tempCell = titleRow.createCell(startIndexColumn);
             tempCell.setCellValue(tempTitle);
             tempCell.setCellStyle(cellStyle);
+            //计算列宽并且设置列宽，按照最大值设置
+            int maxStrLen = getMaxLengthInColumn(insertData,i);
+            maxStrLen = maxStrLen < tempTitle.length() ? tempTitle.length() : maxStrLen;
+            System.out.println(maxStrLen);
+            sheet.setColumnWidth(startIndexColumn,520*maxStrLen);
         }
 
         int allCellCount = titleName.length;//一共有多少列,列按照标题来
         int startRowIndex = startY+1;//内容的起始行
         int allRow = insertData.size();//内容一共有多少行
+
+        CellStyle tempCellStyle = wb.createCellStyle();
+        tempCellStyle.setAlignment(HorizontalAlignment.CENTER);//让文本居中
 
         for (int row = 0; row < allRow; row++) {//遍历所有的内容
             Row tempRow = sheet.createRow(row+startRowIndex);
@@ -66,6 +80,7 @@ public class ExcelUtils {
                     break;
                 }
                 Cell targetCell = tempRow.createCell(cell+startX);//创建列，设置列。
+                targetCell.setCellStyle(tempCellStyle);//让文本居中
                 targetCell.setCellValue(tempData.get(cell));
             }
         }
@@ -79,5 +94,98 @@ public class ExcelUtils {
         } catch (Exception e) {
             throw new BaseException(StaticString.WARNING_TYPE,"生成的excel写入文件失败！");
         }
+    }
+
+    /**
+     * 在excel内容数据中找到当前列的最长字符串长度
+     * @param data excel内容数据
+     * @param column 指定列
+     * @return 当前列最长字符串长度
+     */
+    public static int getMaxLengthInColumn(List<List<String>> data ,int column){
+        int max = 0;
+        for (List<String> tempData : data) {
+            if(tempData.size() <= column){
+                continue;
+            }
+            String tempStr = tempData.get(column);
+            if(tempStr == null){
+                continue;
+            }
+            int tempLen = tempStr.length();
+            max = max < tempLen ? tempLen : max;
+        }
+        return max;
+    }
+
+    /**
+     * 如果Cell为时间时，就通过默认的时间格式获取值
+     * @param cell 目标Cell
+     * @return 返回目标Cell字符串
+     */
+    public static String getCellValue(Cell cell){
+        return getCellValue(cell,format);
+    }
+
+    /**
+     * 获取Cell的值，自动判断类型
+     * @param cell 目标Cell
+     * @return 返回字符串
+     */
+    public static String getCellValue(Cell cell,SimpleDateFormat format) {
+        if(cell == null){//如果为空就返回Null
+            return null;
+        }
+        String o = null;
+        int cellType = cell.getCellType();//获取当前Cell的类型
+        switch (cellType) {
+            //这里有个技巧，就是只判断特殊的几种类型，其它的类型就直接通过default处理。
+            case Cell.CELL_TYPE_ERROR:// 5 当该单元格数据 ERROR 的时候,(故障)
+                break;
+            case Cell.CELL_TYPE_BLANK:// 3 当该单元格没有数据的时候
+                o = "";
+                break;
+            case Cell.CELL_TYPE_NUMERIC:// 0 当该单元格数据为数字的时候
+                o = getValueOfNumericCell(cell,format);
+                break;
+            case Cell.CELL_TYPE_FORMULA:// 2 当该单元格数据为公式的时候
+                try {
+                    o = getValueOfNumericCell(cell,format);
+                } catch (IllegalStateException e) {
+                    o = cell.getRichStringCellValue().toString();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                o = cell.getRichStringCellValue().toString();
+        }
+        return o;
+    }
+
+    /**
+     * 获取日期类型Cell的字符串值，通过默认的日期Pattern
+     * @param cell 目标Cell
+     * @return 返回目标Cell字符串
+     */
+    private static String getValueOfNumericCell(Cell cell) {
+        return getValueOfNumericCell(cell,format);
+    }
+
+    /**
+     * 获取日期类型Cell的字符串值
+     * @param cell 目标Cell
+     * @param format 日期Pattern
+     * @return 返回目标Cell字符串
+     */
+    private static String getValueOfNumericCell(Cell cell, SimpleDateFormat format) {
+        Boolean isDate = DateUtil.isCellDateFormatted(cell);
+        String o = null;
+        if (isDate) {
+            o = format.format(cell.getDateCellValue());
+        } else {
+            o = String.valueOf(cell.getNumericCellValue());
+        }
+        return o;
     }
 }
